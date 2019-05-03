7fe3ce9d796d8f6250df2c912c321dcb74ef08eb
Thibault Marchal <thibault.marchal@epita.fr>
2019-05-03 10:55:53 +0200

    idea intellij

---
+++ .IntelliJIdea2019.1/system/tmp/intellij-git-askpass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/opt/intellij-idea-ue-eap/jre64/bin/java" -cp "/opt/intellij-idea-ue-eap/plugins/git4idea/lib/git4idea-rt.jar:/opt/intellij-idea-ue-eap/lib/xmlrpc-2.0.1.jar:/opt/intellij-idea-ue-eap/lib/commons-codec-1.10.jar:/opt/intellij-idea-ue-eap/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp "$@"
